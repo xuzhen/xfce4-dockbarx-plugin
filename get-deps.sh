9aacf9d6c2f52eaa7d7a61b3be0a86361dceee96
poikilos <7557867+poikilos@users.noreply.github.com>
2021-03-14 12:47:42 -0400

    Improve an apt error message.

--- get-deps.sh
+++ get-deps.sh
@@ -7,4 +7,7 @@ if [ -f "`command -v apt`" ]; then
     ./get-deps-from-apt.sh
 elif [ -f "`command -v apt-get`" ]; then
     ./get-deps-from-apt.sh --command apt-get
+else
+    echo "Error: Only apt and apt-get are implemented but neither are present. See README.md for what dependencies to install manually."
+    exit 1
 fi
